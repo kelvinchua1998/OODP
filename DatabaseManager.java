6a11b35796ac5e6e635ea8c241576778123612b9
Melvin Chua <chua0946@e.ntu.edu.sg>
2020-11-07 18:42:33 +0800

    fix database

--- DatabaseManager.java
+++ DatabaseManager.java
@@ -3,6 +3,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DatabaseManager {
