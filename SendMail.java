ef8380ae0f60201f4ee123cccdad2018a0ccf30d
Melvin Chua <chua0946@e.ntu.edu.sg>
2020-11-14 12:19:44 +0800

    send gmail function for notification

--- SendMail.java
+++ SendMail.java
@@ -10,6 +10,39 @@ import javax.mail.internet.MimeMessage;
 
 public class SendMail {
 
+	public void sendgmail(String senderGMail,String senderGMailUsername, String senderGMailPassword, String RecipientEmail){
+		final String username = senderGMailUsername;
+		final String password = senderGMailPassword; // to be added
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+
+		try {
+			
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(senderGMail));
+			message.setRecipients(Message.RecipientType.TO,InternetAddress.parse(RecipientEmail)); // to be added an email addr
+			message.setSubject("Testing Subject");
+			message.setText("Dear Mail Crawler,"+ "\n\n No spam to my email, please!");
+
+			Transport.send(message);
+
+			System.out.println("Done");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 	public static void main(String[] args) {
 
 		final String username = "melvinchuaqwerty@gmail.com"; // to be added
