17e23c3c6017e3e688bf275852ba80a3d031373d
kelvinchua1998 <kelvin_chua_1998@hotmail.com>
2020-11-15 15:10:26 +0800

    removed admin id

--- Admin.java
+++ Admin.java
@@ -5,20 +5,18 @@ import java.util.ArrayList;
 
 public class Admin extends User implements Serializable{
     private static final long serialVersionUID = 1L;
-    int adminID;
+    
 
-    public Admin(String username, String password,int adminID){
-        super(username,password,"admin",1);
+    public Admin(String username, String password){
+        super(username,password,"admin");
 
         User userObj = new User(username, password, "admin");
 		DatabaseManager databaseManager = new DatabaseManager();
         databaseManager.adduser(userObj);
-
-        this.adminID = adminID;
     }
 
     public static void main(String[] args) {
-        Admin adminObj = new Admin("admin", "admin", 1,);
+        Admin adminObj = new Admin("admin", "admin");
         ArrayList<Admin> adminList = new ArrayList<Admin>();
 
         adminList.add(adminObj);
